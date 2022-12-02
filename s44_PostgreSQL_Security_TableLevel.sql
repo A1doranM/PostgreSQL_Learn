b3af61cef49d67bb062d1a3e7b33f1339461550d
A1doranM <vaselisk1997@gmail.com>
2022-12-02 12:19:44 +0200

    initial

--- s44_PostgreSQL_Security_TableLevel.sql
+++ s44_PostgreSQL_Security_TableLevel.sql
@@ -17,3 +17,7 @@ GRANT permission_name ON ALL TABLES in SCHEMA [schema_name] TO [role_name];
 -- Give sales SELECT ALL on tables in schema -> public;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO sales;
+
+-- Give tech INSERT to specific table
+
+GRANT INSERT ON TABLE employees TO tech;

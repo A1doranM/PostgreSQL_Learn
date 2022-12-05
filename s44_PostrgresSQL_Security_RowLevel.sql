a0da12083021eac077b815770af43e3a5baefad0
A1doranM <vaselisk1997@gmail.com>
2022-12-05 13:26:57 +0200

    L - 579

--- s44_PostrgresSQL_Security_RowLevel.sql
+++ s44_PostrgresSQL_Security_RowLevel.sql
@@ -46,3 +46,39 @@ SELECT * FROM jobs WHERE (max_salary >= 10000 OR max_salary >= 4000);
 DROP POLICY [policy_name]
 
 DROP POLICY p_jobs_sales_min_salary_4000 ON jobs;
+
+-- create policy using for specific user
+
+CREATE POLICY rls_t_users_data_by_username ON t_user_data
+FOR ALL TO PUBLIC
+using (username=current_user);
+
+-- Row level security for application users
+
+/*
+
+    1. While creating policies for users we have used current_user and matched it with the
+       user entry present in the table.
+
+    2. But there are cases where there are too many users, like web applications, and it`s not
+       feasible to create and explicit role for each application user.
+
+    3. Our objective remains the same:
+       a user should only be able to view their own data and not others
+
+    4. One solution may be:
+
+        - Instead of using current_user, we can change our policy to use a session variable.
+        - Session variables can be initialized each time a new user tries to see data.
+
+*/
+
+-- Create policy that uses session variable
+
+CREATE POLICY rls_t_users_data_by_username ON t_user_data
+FOR all TO public
+USING (username=current_setting('rls.username'));
+
+-- set session variable
+
+set rls.username = "tech";

b937d2abde38e9d3f86a50a7c0abeb06b2bd7ecc
A1doranM <vaselisk1997@gmail.com>
2022-12-05 13:32:26 +0200

    L - 583

---
+++ s44_PostrgresSQL_Security_InspectPermissions.sql
@@ -0,0 +1,16 @@
+--  in psql tap: \z table_name;
+
+/*
+
+Before permission name you see something like "arwd/"
+it means what this privilege can do.
+
+a: This append for the INSERT clause.
+r: This append for the SELECT clause.
+w: This append for the UPDATE clause.
+d: This append for the DELETE clause.
+D: This append for the TRUNCATE clause.
+x: This append for the references.
+t: This append for the triggers.
+
+*/

9a93f086a49b3e1fcd510e1824a2cbcfbaf0eeaa
A1doranM <vaselisk1997@gmail.com>
2022-12-12 16:46:08 +0200

    L - 605 - 610

--- 46-PostgreSQLInternals/s46_PostgreSQLInternals.sql
+++ 46-PostgreSQLInternals/s46_PostgreSQLInternals.sql
@@ -58,9 +58,78 @@ select
 
 select version();
 
+-- View privileges information across database, tables, columns
 
+-- Check privileges for current_user
 
+select
+    has_database_privilege('hr', 'create'),
+    has_schema_privilege('public', 'USAGE'),
+    has_table_privilege('movies', 'INSERT'),
+    has_any_column_privilege('movies', 'SELECT');
 
+-- Checking privilege of a particular user and for a particular column
+
+select
+    has_column_privilege('postgres', 'movies', 'movie_name', 'UPDATE');
+
+-- View current timezone information
+
+select current_setting('timezone');
+
+-- File access function
+
+select pg_stat_file('/etc/aliases');
+
+-- Read file contents
+
+select pg_read_file('/etc/aliases');
+
+-- List content of a directory
+
+select pg_ls_dir('/tmp');
+
+-- Show all running queries
+
+select
+    pid,
+    age(clock_timestamp(), query_start),
+    usename as run_by_user_name,
+    query as running_query
+from pg_stat_activity
+where
+    query != '<IDLE>'
+    AND query NOT ILIKE '%pg_stat_activity%'
+order by query_start desc;
+
+-- How to kill running query
+
+select pg_cancel_backend([PROCESS_ID]);
+
+select pg_cancel_backend(17432);
+
+select pg_terminate_backend([PROCESS_ID]);
+
+-- Check live and dead rows in tables
+
+-- Live rows - rows currently in use
+-- Dead rows - deleted rows
+
+select
+    relname,
+    n_live_tup,
+    n_dead_tup
+from pg_stat_user_tables
+
+-- The file layout of PostgreSQL tables
+
+-- location of the PostgreSQL data_directory
+
+show data_directory;
+
+-- where is the files for particular DB located
+
+select pg_relation_filepath('employees');
 
 
 

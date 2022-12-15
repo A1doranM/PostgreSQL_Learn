47ba2e27e47ec87c91b9b90aa7ec34f7355d4071
A1doranM <vaselisk1997@gmail.com>
2022-12-15 13:06:28 +0200

    L - 619-623

--- 47-ManagingTables/s47-619-623.sql
+++ 47-ManagingTables/s47-619-623.sql
@@ -36,3 +36,91 @@ SELECT * FROM generate_series(1,4000); -- 2152 kB
 UPDATE table_vacuum
     SET id = id + 1; -- 11 MB !!!!
 
+-- Autovacuum process
+
+/*
+
+    1. When we have a large number of rows gets deleted in a table, PostgreSQL launches the VACUUM automatically.
+    2. Autovacuum process is enable by default.
+    3. Autovacuum runs in the background. But you can check activity by a query
+    4. By default, the Autovacuum runs every minute.
+    5. Autovacuum does not delete dead rows.
+
+*/
+
+-- 1. Lets view the autovacuum process by query
+
+SELECT
+    relname,
+    last_vacuum,
+    last_autovacuum,
+    vacuum_count,
+    autovacuum_count,
+    last_analyze,
+    last_autoanalyze
+FROM
+    pg_stat_all_tables
+WHERE
+    relname = 'table_vacuum';
+
+-- 2. Recovering unused space using VACUUM
+
+VACUUM [FULL] [FREEZE] [VERBOSE] [ table_name ];
+ANALYZE table_name [(col1, col2, col3)];
+
+/*
+    FULL - all unused space and requires an exclusive lock on each table that is vacuumed
+
+    FREEZE - Optional, the turples are aggressively frozen when the table is vacuumed
+
+    VERBOSE - Optional, am activity report will be printed
+
+    table_name - Optional, is not specified all tables in the database will be vacuumed
+
+    col1, col2 - Optional, same as table_name but for analyze.
+*/
+
+VACUUM VERBOSE table_vacuum;
+
+-- 2. Lets view the autovacuum/vacuum processes by query
+
+SELECT
+    relname,
+    last_vacuum,
+    last_autovacuum,
+    vacuum_count,
+    autovacuum_count,
+    last_analyze,
+    last_autoanalyze
+FROM
+    pg_stat_all_tables
+
+-- Vacuum create new table copy but without dead rows.
+
+---------------------------------------------------------------------
+
+-- Generated Columns
+
+-- Generated columns it is columns computed based on values from another
+-- columns.
+
+CREATE TABLE t
+(
+    width real,
+    height real,
+    area real GENERATED ALWAYS AS (width * height) STORED
+);
+
+-- The value of area is computed at row creation time;
+
+SELECT * FROM t;
+
+-- Lets insert data
+
+INSERT INTO t (width, height) VALUES (2, 2);
+
+-- Or update data
+
+UPDATE t
+SET width = 10
+WHERE width = 1;

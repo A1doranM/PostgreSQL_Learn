d9292374487370530a3fcb47e573080ee3ef6b3c
A1doranM <vaselisk1997@gmail.com>
2023-01-23 20:56:12 +0200

    L - 257-261

--- 23_indexes/s23_query_optimizer_L-257-261.sql
+++ 23_indexes/s23_query_optimizer_L-257-261.sql
@@ -64,4 +64,35 @@
 
             Bitmap Index Scan   builds a memory bitmap of where tuples that satisfy the statement clauses
 
+    -- Join Nodes
+
+    Types:
+    1. Inner table - build a hash table from the inner table, keyed by the join key.
+    2. Outer table - then scan the outer table, checking if a corresponding value is present.
+
+    SHOW work_mem
+    ORDER BY, DISTINCT, AND MERGE JOINS
+
+    HASH table -> hash joins, hash-based aggregation and hash-based processing IN subqueries
+
+    e.g.
+    SELECT * FROM orders where order_id IN ('');
+
+    -- Merge Join
+
+    Joins two children already sorted by their shared join key. This only needs to scan
+    each relation once, but both inputs need to be sorted by the join key first (or scanned
+    in a way that produces already-sorted output like and index scan matching the required sort order)
+
+    -- Nested Loop
+
+    For each row in the outer table, iterate through all the rows in the inner table and see
+    if they match the join condition. If the inner relation can be scanned with an index, that can
+    improve the performance of a Nested Loop Join. tThis is generally and inefficient way to process joins
+    but is always available and sometimes mey be the only option
+
+    e.g.
+    EXPLAIN SELECT * FROM orders
+    NATURAL JOIN customers;
+
 */

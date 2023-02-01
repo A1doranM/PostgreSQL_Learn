97655edc5aa74a886b65f640178079d072b4e424
A1doranM <vaselisk1997@gmail.com>
2023-02-01 19:05:05 +0200

    L - 269

--- 23_indexes/s23_query_cost_model_L-269-.sql
+++
@@ -1,4 +0,0 @@
--- The Query cost model
-/*
-
-*/
---
+++ 23_indexes/s23_query_cost_model_L-269.sql
@@ -0,0 +1,21 @@
+-- The Query cost model
+/*
+
+    -- The more bigger query the more workers PostgreSQL apply to it.
+
+    SHOW max_parallel_workers_per_gather
+    SET max_parallel_workers_per_gather TO 0;
+
+    SELECT pg_relation_size('t_big')
+
+    SHOW seq_page_cost;
+
+    SHOW cpu_tuple_cost;
+
+    SHOW cpu_operator_cost;
+
+    -- cost formula
+
+    pg_relation_size * seq_page_cost + total_number_of_table_records * cpu_tuple_cost
+    + total_number_of_table_records * cpu_operator_cost
+*/
